a657fa87d73216e2e134a69eafe2262a5f4efc22
Hachiko <alextpssio@gmail.com>
2024-06-18 18:32:01 +0200

    Ajoutez des fichiers projet.

---
+++ GestionnaireOeufs/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GestionnaireOeufs
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GestionnaireOeufs/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionnaireOeufs
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
